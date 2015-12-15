6c6ea784d20de548165c997d441e6e5452eadbcc
joelauerbach <joel@artsymail.com>
2015-12-15 12:14:46 -0500

    Update stories.js

--- public/stories.js
+++ public/stories.js
@@ -1,50 +1,50 @@
 var STORIES = {
   "January 2015": {
     label: 'Singapore',
-    copy: 'Lorem Ipsum'
+    copy: 'It’s hardly surprising that a city with such economic might as Singapore would attract a hefty art scene. Major investments have been made in the country’s cultural infrastructure in recent years, including the opening this November of the National Gallery Singapore, the city-state’s first major art museum.'
   },
   "February 2015": {
     label: 'Los Angeles',
-    copy: 'Lorem Ipsum 2'
+    copy: 'L.A.’s recent mass-influx of artists has perhaps drawn the most fervent art-world attention in recent years—major international galleries have flooded in as well. And, in September, the city’s biggest artistic benefactors, Eli and Edythe Broad, opened The Broad, a $140 million new home for their collection, to critical acclaim.'
   },
   "March 2015": {
     label: 'New York',
-    copy: 'Lorem Ipsum 3'
-  }, // Hong Kong
+    copy: 'It’s hard to find an art-related stat in which New York City fails to come out on top. The city is home to more galleries than any other. It has more major art institutions than any other. And its marketplace is bigger than any other—by a large measure.'
+  }, // Hong Kong //Art Basel christened Hong Kong a major art-world capital in 2013 when they purchased ART HK. (The fair moved to March this year, under its new moniker.) The city serves as Asia’s economic hub, making it the region’s natural art market epicenter.
   "April 2015": {
-    label: 'Sao Paulo',
-    copy: 'Lorem Ipsum'
-  }, // Brussels
+    label: 'São Paulo',
+    copy: 'Though 2015 did not include an edition of the ever-influential São Paulo Biennial, Brazil’s financial center remains South America’s first city for art. That’s thanks to April’s SP–Arte and a host of influential galleries such as Mendes Wood DM and Galeria Nara Roesler. '
+  }, // Brussels //Known for its significant collector base—likely the world’s highest concentration in any one metropole—and their penchant for emerging art, Brussels boasts an enviable bunch of galleries. Each April Art Brussels welcomes those outfits and a host of others from around the world to Belgium.
   "May 2015": {
     label: 'Venice',
-    copy: 'Lorem Ipsum'
+    copy: 'The art world touched down in Venice once again this May for the 56th Biennale, with its central exhibition, “All the World’s Futures,” headed up this time by Haus der Kunst director Okwui Enwezor. For the past 120 years, the biennale has been a touchstone for artistic progress, and this year was no different.'
   },
   "June 2015": {
     label: 'Zurich',
-    copy: 'Lorem Ipsum'
-  },
+    copy: 'Each June, the art world stops off in Zürich for its annual gallery weekend ahead of the art market megalith that is Art Basel in Basel. The latter’s location on the borders of Switzerland, France, and Germany (and its resulting status as a freeport) place it in the midst of Europe’s deepest pockets.'
+  }, //Basel
   "July 2015": {
     label: 'Beijing',
-    copy: 'Lorem Ipsum'
+    copy: 'Shanghai may host mainland China’s biggest market events, but Beijing remains the first city for artists in the world’s second-most-powerful economy. The scene is centered in the 798 Arts District, where major institutions like the Ullens Center for Contemporary Art (UCCA) abut a growing number of dealers.'
   },
   "August 2015": {
     label: 'Detroit',
-    copy: 'Lorem Ipsum'
+    copy: 'Detroit has been lauded as America’s Berlin for the past several years. And the Motor City continues to attract alternative spaces looking to jump the coasts’ hustle (and steep rents). Detroit also serves as a stand-in on this list for the many emerging art capitals worldwide, from Mexico City to Mumbai, Bogotá to Beirut, and beyond that are in the midst of explosions of artistic activity and may well break into this list in 2016. '
   },
   "September 2015": {
     label: 'Berlin',
-    copy: 'Lorem Ipsum'
-  }, // Istanbul
+    copy: 'Known first as a home to artists and artistic production, Berlin is in many respects the conceptual soul of contemporary art. While April’s Gallery Weekend and September’s Berlin Art Week attract the biggest amount of attention each year, Berlin remains a key locale across the calendar for anyone in the art world keen to know what’s next.'
+  }, // Istanbul //dOCUMENTA (13)’s beloved artistic director Carolyn Christov-Bakargiev curated the 14th Istanbul Biennial this year, drawing all eyes to the Bosphorus’s shores this fall. The city’s unique position between East and West continues to make it a rich context for art to unfold within.
   "October 2015": {
     label: 'London',
-    copy: 'Lorem Ipsum'
-  }, // Paris
+    copy: 'Over the past decade, the London contemporary art scene has taken an upward turn, thanks in no small part to the city’s ability to attract the global elite. The Frieze Art Fair brings the balance of the art world to London each October. And, from the Tate to the Whitechapel, the ICA to the RA, the city’s museums at least rival those of New York.'
+  },
   "November 2015": {
-    label: '',
-    copy: 'Lorem Ipsum'
+    label: 'Paris',
+    copy: 'Paris welcomes the art world each October for the Grande Dame of art fairs, FIAC—and, this year, impressive young upstart Paris Internationale. Meanwhile, its host of influential gallerists and institutions bolster the critical conversation year-round.'
   },
   "December 2015": {
-    label: 'Miami',
-    copy: 'Lorem Ipsum'
+    label: 'Miami + Miami Beach',
+    copy: 'Each December, an avalanche of attention hits South Beach thanks to Art Basel in Miami Beach. The city serves as the art market’s gateway to Latin and South America, and numerous satellite fairs and fêtes have joined the 14-year-old main event to mark a tropical send-off to the art-world calendar.'
   },
 };
